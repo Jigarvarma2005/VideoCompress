23df19965e118ce84d15e1958fedf2d76a8900f6
Mr. Developer <77911154+MrBotDeveloper@users.noreply.github.com>
2021-03-12 19:22:54 +0530

    Update ffmpeg.py

--- bot/helper_funcs/ffmpeg.py
+++ bot/helper_funcs/ffmpeg.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# (c) Shrimadhav U K | @AbirHasan2005 | Mrvishal2k2
+# (c) Shrimadhav U K | @AbirHasan2005 | @Mrvishal2k2 | @MrBotDeveloper
 
 # the logging things
 import logging
@@ -17,16 +17,18 @@ import re
 import json
 import subprocess
 import math
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from bot.helper_funcs.display_progress import (
   TimeFormatter
 )
 from bot.localisation import Localisation
 from bot import (
     FINISHED_PROGRESS_STR,
-    UN_FINISHED_PROGRESS_STR
+    UN_FINISHED_PROGRESS_STR,
+    DOWNLOAD_LOCATION
 )
 
-async def convert_video(video_file, output_directory, total_time, bot, message, chan_msg):
+async def convert_video(video_file, output_directory, total_time, bot, message, target_percentage, isAuto):
     # https://stackoverflow.com/a/13891070/4723940
     out_put_file_name = output_directory + \
         "/" + str(round(time.time())) + ".mp4"
@@ -44,16 +46,35 @@ async def convert_video(video_file, output_directory, total_time, bot, message, 
       "-i",
       video_file,
       "-c:v", 
-      "libx265",
-      "-vtag",
-      "hvc1",
+      "libx264",
       "-preset", 
       "ultrafast",
+      "-tune",
+      "film",
       "-c:a",
       "copy",
       out_put_file_name
     ]
-#Done !!
+    if not isAuto:
+      filesize = os.stat(video_file).st_size
+      calculated_percentage = 100 - target_percentage
+      target_size = ( calculated_percentage / 100 ) * filesize
+      target_bitrate = int(math.floor( target_size * 8 / total_time ))
+      if target_bitrate // 1000000 >= 1:
+        bitrate = str(target_bitrate//1000000) + "M"
+      elif target_bitrate // 1000 > 1:
+        bitrate = str(target_bitrate//1000) + "k"
+      else:
+        return None
+      extra = [ "-b:v", 
+                bitrate,
+                "-bufsize",
+                bitrate
+              ]
+      for elem in reversed(extra) :
+        file_genertor_command.insert(10, elem)
+    else:
+       target_percentage = 'auto'
     COMPRESSION_START_TIME = time.time()
     process = await asyncio.create_subprocess_exec(
         *file_genertor_command,
@@ -73,7 +94,7 @@ async def convert_video(video_file, output_directory, total_time, bot, message, 
     isDone = False
     while process.returncode != 0:
       await asyncio.sleep(3)
-      with open("/app/downloads/progress.txt",'r+') as file:
+      with open("/app/DOWNLOADS/progress.txt",'r+') as file:
         text = file.read()
         frame = re.findall("frame=(\d+)", text)
         time_in_us=re.findall("out_time_ms=(\d+)", text)
@@ -103,28 +124,28 @@ async def convert_video(video_file, output_directory, total_time, bot, message, 
         if difference > 0:
           ETA = TimeFormatter(difference*1000)
         percentage = math.floor(elapsed_time * 100 / total_time)
-        progress_str = "📊 <b>Progress:</b> {0}%\n[{1}{2}] \n\n© @UniversalBotsUpdate".format(
+        progress_str = "📊 <b>Progress:</b> {0}%\n[{1}{2}]".format(
             round(percentage, 2),
             ''.join([FINISHED_PROGRESS_STR for i in range(math.floor(percentage / 10))]),
             ''.join([UN_FINISHED_PROGRESS_STR for i in range(10 - math.floor(percentage / 10))])
             )
-        stats = f'📦️ <b>Converting To H256 </b>\n\n' \
-                f'⏰️ <b>TimeLeft:</b> {ETA}\n\n' \
+        stats = f'📦️ <b>Compressing</b> {target_percentage}%\n\n' \
+                f'⏰️ <b>ETA:</b> {ETA}\n\n' \
                 f'{progress_str}\n'
         try:
           await message.edit_text(
-            text=stats
+            text=stats,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [ 
+                        InlineKeyboardButton('❌ Cancel ❌', callback_data='fuckingdo')
+                    ]
+                ]
+            )
           )
         except:
             pass
         
-        try:
-          await chan_msg.edit_text(
-            text=stats
-          )
-        except:
-            pass
-
     # Wait for the subprocess to finish
     stdout, stderr = await process.communicate()
     #if( not isDone):

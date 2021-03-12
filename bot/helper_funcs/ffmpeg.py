ae74daf4d74c5ce8365c7dd5ea85023cfdedc1ea
Jigar varma <71844973+Technical-Jigar@users.noreply.github.com>
2021-03-12 19:29:55 +0530

    Update ffmpeg.py

--- bot/helper_funcs/ffmpeg.py
+++ bot/helper_funcs/ffmpeg.py
@@ -94,7 +94,7 @@ async def convert_video(video_file, output_directory, total_time, bot, message, 
     isDone = False
     while process.returncode != 0:
       await asyncio.sleep(3)
-      with open("/app/DOWNLOADS/progress.txt",'r+') as file:
+      with open("/app/downloads/progress.txt",'r+') as file:
         text = file.read()
         frame = re.findall("frame=(\d+)", text)
         time_in_us=re.findall("out_time_ms=(\d+)", text)
